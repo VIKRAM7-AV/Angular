b0197be1916a67ad0e7d695105c3c8ab1cd8c882
VIKRAM <vikramria195@gmail.com>
2026-02-14 14:15:48 +0530

    Data Binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('TestApp');
+  title="Angular nkknJs";
+
+  getName (){
+    return "Vikram Swe"
+  };
+
+  ImageURl="https://picsum.photos/200/300";
+  Imagealt="Okk"
+
 }
